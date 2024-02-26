8b22dfebcfda71cd8c509078c619dd5f59caa32f
Omar Elmoez <omarelmoez2000@gmail.com>
2024-02-26 14:35:28 +0200

    using defer

--- src/loaders/Events.tsx
+++ src/loaders/Events.tsx
@@ -1,13 +1,22 @@
-import { json } from "react-router-dom";
+import { json, defer } from "react-router-dom";
+import Data from "../types/data.types";
 
-async function EventsLoader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
     throw json({ message: "Could not fetch events." }, { status: 500 });
   }
 
-  return response;
+  const resData = await response.json() as Data;
+  return resData.events;
+}
+
+function EventsLoader() {
+  return defer({
+    events: loadEvents(),
+  })
 }
 
 export default EventsLoader;
+
--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,11 +1,18 @@
-import { useLoaderData } from "react-router-dom";
+import { Await, useLoaderData } from "react-router-dom";
 import { EventsList } from "../components";
 import Data from "../types/data.types";
+import { Suspense } from "react";
+import Event from "../types/event.types";
 
 function EventsPage() {
   const { events } = useLoaderData() as Data;
-
-  return <EventsList events={events} />;
+  return (
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(events) => <EventsList events={events as Event[]} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
